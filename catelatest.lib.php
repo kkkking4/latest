8bd2969d0cc1b0abcfbf732412bb5a6c7e3b0e4b
kkkking4 <kkkking4@naver.com>
2019-03-14 16:02:24 +0900

    카테고리별 노출
    카테고리별 구분, 지정해서 표출

--- catelatest.lib.php
+++ catelatest.lib.php
@@ -1,23 +1,20 @@
 <?php
 global g5; 
+
+//상단에 아래 코드 추가
 list($bo_table, $category) = explode("|", $bo_table);
 if($category) {
     $cat = explode(",", $category);
     $cats = " AND ca_name IN('".implode("', '", $cat)."') ";
 }
-//코드 추가
-
-
 
 //$sql = " select * from {$tmp_write_table} where wr_is_comment = 0 order by wr_num limit 0, {$rows} "; 라인을 찾아
-
 
 $sql = " select * from {$tmp_write_table} where wr_is_comment = 0".$cats." order by wr_num limit 0, {$rows} ";
 
 //수정하면 카테고리를 지정해 노출할 수 있다.
-
+?>
+<?php
  //메인 페이지에서
-
-<?php echo latest("스킨", "게시판|카테고리1,카테고리2", 5, 25); ?>
-//게시판 아이디 | 카테고리를 구분해서 넣고 함수를 호출
+echo latest("스킨", "게시판|카테고리1,카테고리2", 5, 25); //게시판 아이디 | 카테고리를 구분해서 넣고 함수를 호출
 ?>
