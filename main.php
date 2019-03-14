0b3c1a2991b34ba3436c1fb5059760af242cbb2d
kkkking4 <kkkking4@naver.com>
2019-03-14 14:04:09 +0900

    노출페이지

---
+++ main.php
@@ -0,0 +1,3 @@
+<?php echo latest_main("main_visual", movie, 30 , 10,"and wr_11='1'","wr_num"); ?>
+<?php echo latest_main("main_visual", movie, 30 , 10,"and wr_12='1'","wr_hit"); ?>
+<?php //여분필드를 최신 메인 글 추출 함수 인자값으로 넘겨서 정렬순서 지정 다양한 정렬순으로 노출 ?>
